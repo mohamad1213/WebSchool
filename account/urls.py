4395045152b5bf51b54b6817ff667177da6d47b4
Mohamad Hatami <hatami391998@gmail.com>
2022-08-10 21:15:50 +0700

    update all

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.loginPage, name="login"),  
-	# path('register/', views.registerPage, name="register"),
+	path('register/', views.registerPage, name="register"),
 	path('logout/', views.logoutUser, name="logout"),
 ]
